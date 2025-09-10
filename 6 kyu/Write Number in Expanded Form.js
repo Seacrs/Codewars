a624f9f40435a05257e60bf8afe83fb7203a5403
Seacrs <138714004+Seacrs@users.noreply.github.com>
2025-09-10 19:37:49 +0200

    Update Write Number in Expanded Form.js

--- 6 kyu/Write Number in Expanded Form.js
+++ 6 kyu/Write Number in Expanded Form.js
@@ -1,3 +1,12 @@
+// Write Number in Expanded Form
+// You will be given a number and you will need to return it as a string in 
+// Expanded Form. For example:
+
+//    12 --> "10 + 2"
+//    45 --> "40 + 5"
+// 70304 --> "70000 + 300 + 4"
+// NOTE: All numbers will be whole numbers greater than 0.
+
 function expandedForm(num) {
   // Your code here
   let arr = String(num).split('');
@@ -11,5 +20,5 @@ function expandedForm(num) {
     }
     expanded.push(str);
   }
-
-  return expanded.map(Number).filter(x => x !== 0).map(String).join(' + ');
+return expanded.map(Number).filter(x => x !== 0).map(String).join(' + ');
+}
