a42c59de5d064f3e8ed6ec86a2f0e49a12f560d5
Seacrs <shemachris072@gmail.com>
2026-02-06 20:46:23 +0200

    Add kata: 5kyu - Convert A Hex String To RGB

---
+++ TypeScript/5 kyu/Convert A Hex String To RGB.ts
@@ -0,0 +1,23 @@
+// When working with color values it can sometimes be useful to extract 
+// the individual red, green, and blue (RGB) component values for a color. 
+// Implement a function that meets these requirements:
+
+//     Accepts a case-insensitive hexadecimal color string as its 
+//     parameter (ex. "#FF9933" or "#ff9933")
+//     Returns a Map<String, int> with the structure 
+//     {r: 255, g: 153, b: 51} where r, g, and b range from 0 through 255
+
+// Note: your implementation does not need to support the shorthand 
+// form of hexadecimal notation (ie "#FFF")
+
+// Example
+// "#FF9933" --> {r: 255, g: 153, b: 51}
+
+export function hexStringToRGB(hexString: string): { r: number, g: number, b: number } {
+    const rgb: string[] = [];
+    for(let i = 1; i < hexString.length; i+=2 ){
+        rgb.push(`${hexString[i]}${hexString[i+1]}`)
+    }
+    const [r, g, b] = rgb.map(el => parseInt(el, 16));
+    return { r: r, g: g, b: b};
+}
