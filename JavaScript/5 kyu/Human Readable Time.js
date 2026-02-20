ecfdb83f5687ff47c2b0ad494645de46823bbcbb
Seacrs <shemachris072@gmail.com>
2026-02-20 13:35:23 +0200

    Add kata: 5kyu - Human Readable Time

---
+++ JavaScript/5 kyu/Human Readable Time.js
@@ -0,0 +1,15 @@
+// Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
+
+// HH = hours, padded to 2 digits, range: 00 - 99
+// MM = minutes, padded to 2 digits, range: 00 - 59
+// SS = seconds, padded to 2 digits, range: 00 - 59
+// The maximum time never exceeds 359999 (99:59:59)
+
+// You can find some examples in the test fixtures.
+
+function humanReadable (seconds) {
+    const h = Math.floor(seconds / 3600);
+    const m = Math.floor((seconds % 3600) / 60);
+    const s = seconds % 60;
+    return`${String(h).padStart(2, '0')}:${String(m).padStart(2, '0')}:${String(s).padStart(2,'0')}`
+}
