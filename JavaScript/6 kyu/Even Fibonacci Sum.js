446c30d424377d301acf58db2bdcb0b5b8d2ddc6
Seacrs <shemachris072@gmail.com>
2026-03-03 14:40:09 +0200

    Add kata: 6kyu - Even Fibonacci Sum

---
+++ JavaScript/6 kyu/Even Fibonacci Sum.js
@@ -0,0 +1,35 @@
+// Give the summation of all even numbers in a Fibonacci sequence up to, 
+// but not including, the number passed to your function. Or, in other 
+// words, sum all the even Fibonacci numbers that are lower than the 
+// given number n (n is not the nth element of Fibonacci sequence) 
+// without including n.
+
+// The Fibonacci sequence is a series of numbers where the next value is 
+// the addition of the previous two values. The series starts with 0 and 1:
+
+// 0 1 1 2 3 5 8 13 21...
+
+// For example:
+
+// 0 --> 0
+// 33 --> 10
+// 25997544 --> 19544084
+
+function fibonacci(max){
+    let prev = 0;
+    let curr = 1;
+
+    let sum = 0;
+    
+    while(curr < max){
+        if(curr % 2 === 0){
+            sum = sum + curr;
+        }
+
+        let nCurr = prev + curr;
+
+        prev = curr;
+        curr = nCurr;
+    }
+    return sum;
+}
